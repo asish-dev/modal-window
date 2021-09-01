c3299dec16d5bb151dddc6d1ce4f51396e8fb65c
Asish Krishna Samanta <asish043@gmail.com>
2021-09-01 21:59:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,5 +8,27 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 console.log(btnsOpenModal);
 
-for(let i=0;i<btnsOpenModal.length;i++)
+const openModal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+for(let i=0;i<btnsOpenModal.length;i++){
     console.log(btnsOpenModal[i]);
+    btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+const closeModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function(e){
+    console.log(e);
+
+    if(e.key === 'Escape' && !modal.classList.contains('hidden'))
+        closeModal();
+});
